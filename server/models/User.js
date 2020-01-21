ecfbc90e011d4f875230c0ff9b80b6d444b5dcce
Dor Ben Lulu <benluludor@yahoo.com>
2020-01-21 15:15:54 +0200

    User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,19 @@
-// aa
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  phoneNumber: Number,
+  age: Number,
+  status: String,
+  desiredRelationship: String,
+  interestedIn: Array,
+  gender: String,
+  picture: String,
+  isCheckedIn: Boolean,
+  isDeleted: Boolean
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = Transaction;
