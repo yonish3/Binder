450fe5877571d908b45e0ab08b6249f8a7169171
Haim Ruso <haimruso23@gmail.com>
2020-01-22 10:18:58 +0200

    get check in getlocation done

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ const errorHandler = require('./server/middlewares/errorHandler/errorHandler')
 const config = require('./config/config')
 const port = 8080
 const dbSetup = require('./server/db/dbSetup')
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    next()
+})
 // app.use(express.static(path.join(__dirname, 'build')))
 app.use("/", api);
+
 // app.use(errorHandler)
 
 // dbSetup.dbSetup()
--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { observer } from 'mobx-react'
-import './App.css';
+import {GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
 
-@observer
-class App extends Component {
+
+
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        HELLO
-      </div>
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
 
-
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBfabs7hjM38iufAEGazLloZUS9t9DfUy8'
+})(MapContainer);
