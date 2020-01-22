fc7815c4e50a3966f8d5f5378a8e70e3c0b005bf
vickiMen <vicki.menashe@gmail.com>
2020-01-23 01:09:17 +0200

    adding again missing files/code from ServerSocket branch

--- src/stores/socketStore.js
+++ src/stores/socketStore.js
@@ -1,25 +1,21 @@
 import { observable, action } from "mobx"
 import dummyData from "./dummyData"
 import socketIOClient  from "socket.io-client"
-
 export class SocketStore{
     endpoint = "localhost:8080"
     socket = socketIOClient(this.endpoint);
     @observable socketId = "";
     @observable coordinates = {}
     @observable nearbyLocations = []
-
+    @observable nearbyUsers = []
     @action openSocket = () => {
         // const ;
         this.socket.emit('userId', '5e270a0e2647322352129dae')
         this.socket.on('userId', (userIdin) => {
             console.log('recived: '+ userIdin)
         })
-        
     }
-
     @action getLocationsNearby = function(coordinates) {
-
         this.socket.emit('GPSlocation', coordinates);
         this.socket.on('locationsArry',  (locationsArry) => {
             console.log('locationsArry',locationsArry)
@@ -27,13 +23,14 @@ export class SocketStore{
           })
     }
     @action getUsersNearMe = (location) => {
-        console.log('before emit locaiton: '+location)
+        console.log('before emit location: '+location)
         this.socket.emit('selectedLocation', location);
-        
+        this.socket.on('usersNearMe',  (usersNearMe) => {
+            console.log('usersNearMe: ' + usersNearMe)
+            this.nearbyUsers = usersNearMe
+          })
     }
-
     @action sendReaction = (reactionObj) => {
         this.socket.emit('reaction', reactionObj)
     }
 }
-
