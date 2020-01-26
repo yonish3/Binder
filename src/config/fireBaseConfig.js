a6c4a66e9dbf44fb7c5da3121e77e7e96a68c4b6
Haim Ruso <haimruso23@gmail.com>
2020-01-26 19:08:39 +0200

    uploaded images!

--- src/config/fireBaseConfig.js
+++ src/config/fireBaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBfabs7hjM38iufAEGazLloZUS9t9DfUy8",
-    authDomain: "binder-1579608819026.firebaseapp.com",
-    databaseURL: "https://binder-1579608819026.firebaseio.com",
-    projectId: "binder-1579608819026",
-    storageBucket: "binder-1579608819026.appspot.com",
-    messagingSenderId: "320654367396",
-    appId: "1:320654367396:web:7ae20fc56bc03fafece431",
-    measurementId: "G-ELK2CBHNG4"
+    apiKey: "AIzaSyD15Hl3UZ8yuiNs3KljCJ0ePQgs6Wf-0ps",
+    authDomain: "binder2-319a5.firebaseapp.com",
+    databaseURL: "https://binder2-319a5.firebaseio.com",
+    projectId: "binder2-319a5",
+    storageBucket: "binder2-319a5.appspot.com",
+    messagingSenderId: "890819851860",
+    appId: "1:890819851860:web:3d880fb18a5d4a8749dfc1",
+    measurementId: "G-F6E8HMJB7G"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
