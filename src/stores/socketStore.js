c61e85953afbadaddd0d71511ca587a64a71d906
Efrat Harel <efratha94@gmail.com>
2020-01-26 11:30:09 +0200

    trying to pull dev

--- src/stores/socketStore.js
+++ src/stores/socketStore.js
@@ -1,6 +1,7 @@
 import { observable, action, computed } from "mobx"
 import dummyData from "./dummyData"
 import socketIOClient  from "socket.io-client"
+
 export class SocketStore{
     endpoint = "localhost:8080"
     socket = socketIOClient(this.endpoint);
